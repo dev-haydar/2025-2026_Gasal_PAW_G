70115c363225ddea5698473c8366cd676efc2f46
dev-haydar <gusgatot12@gmail.com>
2025-10-03 14:12:39 +0700

    soal1.php

--- Modul-2/23-105_Ahmad Haydar Al Abror/soal1.php
+++ Modul-2/23-105_Ahmad Haydar Al Abror/soal1.php
@@ -1,1 +1,17 @@
+<?php
+    $matkul = ["PAW","ALPRO","DPW","STRUKDAT","JARKOM","PTI","PSBF","RPL"];
+    $praktikum = ["JARKOM","PAW"];
+
+    for($i = 0; $i < count($matkul);$i++){
+        if($matkul[$i] == $praktikum[0]){
+            echo "Saya sedang mengambil matkul . $praktikum[0] . termasuk praktikumnya <br>";
+        }elseif($matkul[$i] == $praktikum[1]){
+            echo "Saya sedang mengambil matkul . $praktikum[1] . termasuk praktikumnya <br>";
+        }elseif($i == 6 || $i == 7){
+            echo "Saya belum mengambil matkul $matkul[$i] <br>";
+        }else{
+            echo "Saya sudah mengambil matkul $matkul[$i] semester lalu <br> ";
+        }
+    }
+?>
 
