2f4e8011cb1172339ea03706fd3490c059d78c7f
dev-haydar <gusgatot12@gmail.com>
2025-10-25 20:27:19 +0700

    Add form validation and error handling

--- Modul-4/23-105_Ahmad Haydar Al Abror/processdata.php
+++ Modul-4/23-105_Ahmad Haydar Al Abror/processdata.php
@@ -1,1 +1,15 @@
 
+<?php 
+require 'validate.inc'; 
+$errors = [];  
+if ($_SERVER['REQUEST_METHOD'] == 'POST') { 
+    if (validateName($_POST, 'surname', $errors)) { 
+        echo "<p style='color:green'>Form submitted successfully 
+with no errors.</p>"; 
+    } else { 
+        require 'form.inc'; 
+    } 
+} else { 
+    require 'form.inc'; 
+} 
+?> 
