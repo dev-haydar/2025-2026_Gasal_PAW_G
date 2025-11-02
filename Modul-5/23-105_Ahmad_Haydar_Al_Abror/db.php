0dc41d023fab4eb75452b0d6753c53740202442d
dev-haydar <gusgatot12@gmail.com>
2025-11-02 20:45:36 +0700

    Update db.php

--- Modul-5/23-105_Ahmad_Haydar_Al_Abror/db.php
+++ Modul-5/23-105_Ahmad_Haydar_Al_Abror/db.php
@@ -1,1 +1,13 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "praktikum_db";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
+?>
 
